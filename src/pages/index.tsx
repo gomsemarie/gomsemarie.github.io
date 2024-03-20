2f4da042c76ac82b65f8e109e4f391bcacf67981
lacolico <gomsemarie@gmail.com>
2024-03-21 08:51:00 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { graphql, PageProps } from "gatsby";
+
+type DataProps = {
+  site: {
+    siteMetadata: {
+      title: string;
+    };
+  };
+};
+
+const IndexRoute = ({ data: { site } }: PageProps<DataProps>) => {
+  return (
+    <main>
+      <h1>{site.siteMetadata.title}</h1>
+    </main>
+  );
+};
+
+export default IndexRoute;
+
+export const query = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
